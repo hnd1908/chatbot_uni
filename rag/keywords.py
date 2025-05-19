2b8d896e4f390a0a6e3f015a98971a9047e5b59c
Nguyễn Duy Hoàng <22520467@gm.uit.edu.vn>
2025-05-19 18:32:41 +0700

    update: make keywords_dict (include its key and value) more precise and detailed

--- rag/keywords.py
+++ rag/keywords.py
@@ -1,94 +1,97 @@
 keywords_dict = {
-    "general": [
-        "An toàn thông tin", "ATTT", "an ninh mạng",
-        "Công nghệ thông tin", "CNTT", "đào tạo CNTT", "hệ thống đào tạo", "phát triển công nghệ",
-        "phần mềm máy tính", "xử lý dữ liệu", "ứng dụng phần mềm", "ứng dụng công nghệ thông tin",
-        "công nghệ web", "thiết bị di động", "phát triển ứng dụng",
-        "Hệ Thống Thông Tin", "HTTT", "Thương Mại Điện Tử",
-        "CSDL", "Khoa học dữ liệu", "KHDL", "quản trị dữ liệu", "phân tích dữ liệu",
-        "máy học", "thống kê", "nhiệm vụ học máy", "dữ liệu lớn", "Big Data", "khai thác dữ liệu",
-        "Khoa học Máy tính", "Trí tuệ nhân tạo", "AI", "đào tạo AI", "nghiên cứu AI",
-        "Thị giác Máy tính", "Computer Vision", "Machine Learning", "Đồ họa máy tính",
-        "Kỹ thuật Máy tính", "KTMT", "thiết kế phần cứng", "phát triển phần cứng", "phần cứng máy tính",
-        "IoT", "Thiết kế vi mạch", "Robot",
-        "Kỹ thuật phần mềm", "KTPM", "kỹ sư phần mềm", "thiết kế phần mềm", "phát triển game",
-        "mạng máy tính IoT", "kết nối IoT",
-        "Thiết kế Vi Mạch", "chế tạo vi mạch", "kiến thức cơ sở ngành vi mạch", "bán dẫn",
-        "cơ hội việc làm vi mạch", "nghề nghiệp vi mạch",
-        "TMĐT", "ứng dụng TMĐT",
-        "ngành hot AI", "Truyền thông Đa phương tiện",
-        "tuyển sinh", "xét tuyển", "tuyển thẳng", "ưu tiên xét tuyển", "Quy chế tuyển sinh", "Bộ GD&ĐT",
-        "Đại học Công nghệ Thông tin", "Olympic quốc tế", "Học sinh giỏi quốc gia",
-        "ngành", "ĐHQG-HCM", "học sinh giỏi", "trường THPT", "hạnh kiểm", "điểm trung bình",
-        "chứng chỉ ngoại ngữ", "kết quả học tập", "thi đánh giá năng lực", "kỳ thi ĐGNL",
-        "kết quả thi tốt nghiệp THPT", "tổ hợp môn", "chứng chỉ quốc tế", "SAT", "ACT", "IELTS",
-        "TOEFL", "JLPT", "chương trình liên kết", "bằng cấp", "chương trình tài năng",
-        "Đường lên đỉnh Olympia", "điểm trung bình THPT", "điểm THPT", "nguyện vọng", "đăng ký",
-        "nộp hồ sơ", "thời gian", "trang thông tin điện tử", "email xác nhận", "hồ sơ giấy",
-        "phiếu đăng ký xét tuyển", "học bạ THPT", "Giấy chứng nhận đạt giải", "kết quả trúng tuyển",
-        "lệ phí xét tuyển", "trường chuyên", "trường năng khiếu",
-        "đội tuyển học sinh giỏi quốc gia", "xét tuyển thẳng", "thành tích học tập", "năng khiếu",
-        "thể thao", "nghệ thuật", "học sinh khá", "điểm xét tuyển", "điểm quy đổi",
-        "Trường Đại học Công nghệ Thông tin", "UIT", "thành lập", "tiền thân", "khẩu hiệu",
-        "địa chỉ", "website", "cơ sở vật chất", "diện tích", "tòa nhà", "Thư viện",
-        "tài liệu", "sách", "lịch sử", "hiệu trưởng", "sinh viên", "chuyên ngành",
-        "khai giảng", "kỷ niệm thành lập", "ngành đào tạo", "Công nghệ Thông tin",
-        "Hệ thống Thông tin", "Khoa học Máy tính", "Kỹ thuật Máy tính", "Kỹ thuật Phần mềm",
-        "Mạng máy tính và Truyền thông dữ liệu", "An toàn Thông tin", "Thương mại Điện tử",
-        "Khoa học Dữ liệu", "Trí tuệ Nhân tạo", "Thiết kế Vi mạch", "sau đại học", "thạc sĩ",
-        "tiến sĩ", "lãnh đạo", "phó hiệu trưởng", "thông tin khoa", "khoa", "cơ cấu tổ chức",
-        "Ban giám hiệu", "bộ môn", "Bộ môn Toán - Lý", "chất lượng cao", "nghiên cứu",
-        "trình độ sau Đại học", "giáo sư", "phó giáo sư", "đại học", "trợ giảng",
-        "tốt nghiệp nước ngoài", "hiệu trưởng qua các thời kỳ", "Dương Anh Đức",
-        "Nguyễn Hoàng Tú Anh", "phó hiệu trưởng qua các thời kỳ",
-        "Nguyễn Tấn Trần Minh Khang", "Nguyễn Lưu Thùy Ngân",
-        "thực tập", "thông tin tuyển dụng", "hội thảo", "seminar", "Ngoại ngữ", "kỹ năng",
-        "tín chỉ", "tốt nghiệp", "điều kiện tốt nghiệp", "khóa luận tốt nghiệp", "môn học",
-        "đk tốt nghiệp", "khối kiến thức", "Thực tập tốt nghiệp và khối kiến thức tốt nghiệp",
-        "Tích lũy tối thiểu", "công nhận tốt nghiệp", "cấp bằng", "giáo dục đại cương",
-        "cơ sở ngành", "chuyên ngành", "tự chọn tự do", "tự chọn", "Kiến thức tốt nghiệp",
-        "đầu ra", "chuẩn đầu ra"
+    "tuyensinh": [
+        "tuyển sinh", "xét tuyển", "tuyển thẳng", "ưu tiên xét tuyển", "Quy chế tuyển sinh",
+        "kết quả thi tốt nghiệp THPT", "thi đánh giá năng lực", "kỳ thi ĐGNL", "điểm xét tuyển",
+        "phiếu đăng ký xét tuyển", "điểm quy đổi", "hồ sơ giấy", "kết quả trúng tuyển",
+        "nguyện vọng", "đăng ký", "nộp hồ sơ", "email xác nhận", "học bạ THPT", 
+        "Giấy chứng nhận đạt giải", "trường chuyên", "trường năng khiếu", "điểm THPT"
     ],
-    "score": [
+    "diem": [
         "diem chuan", "diem trung tuyen", "muc diem", "diem san", "điểm chuẩn",
-        "điểm trúng tuyển", "mức điểm", "điểm sàn", "kết quả thi THPT",
-        "điểm thi đánh giá năng lực", "thpt", "dgnl", "đgnl", "điểm thi THPT",
-        "thi thpt", "điểm đh", "điểm đại học"
+        "điểm trúng tuyển", "mức điểm", "điểm sàn", "điểm thi đánh giá năng lực",
+        "điểm thi THPT", "điểm đh", "điểm đại học", "kết quả thi THPT", "thpt", "thi thpt",
+        "dgnl", "đgnl"
     ],
     "attt": [
-        "An toàn thông tin", "ATTT", "an ninh mạng", "bảo mật", "phần mềm độc hại", "phòng tránh", "kiểm tra an ninh mạng"
+        "An toàn thông tin", "ATTT", "an ninh mạng", "bảo mật", "phần mềm độc hại", 
+        "phòng tránh", "kiểm tra an ninh mạng", "bảo mật thông tin"
     ],
     "cntt": [
-        "Công nghệ thông tin", "CNTT", "phát triển công nghệ", "xử lý dữ liệu", "ứng dụng phần mềm", "bảo mật thông tin", "kỹ sư CNTT"
+        "Công nghệ thông tin", "CNTT", "phát triển công nghệ", "xử lý dữ liệu", 
+        "ứng dụng phần mềm", "kỹ sư CNTT"
     ],
     "httt": [
-        "Hệ Thống Thông Tin", "HTTT", "quản lý", "phân tích hệ thống", "thiết kế hệ thống", "ERP", "thương mại điện tử"
+        "Hệ Thống Thông Tin", "HTTT", "quản lý", "phân tích hệ thống", 
+        "thiết kế hệ thống", "ERP", "thương mại điện tử"
     ],
     "khdl": [
-        "Khoa học dữ liệu", "KHDL", "phân tích dữ liệu", "máy học", "dữ liệu lớn", "khai thác dữ liệu", "ứng dụng dữ liệu"
+        "Khoa học dữ liệu", "KHDL", "phân tích dữ liệu", "máy học", "dữ liệu lớn", 
+        "khai thác dữ liệu", "ứng dụng dữ liệu"
     ],
     "khmt": [
-        "Khoa học Máy tính", "KHMT", "Trí tuệ nhân tạo", "AI", "máy học", "thị giác máy tính", "xử lý ngôn ngữ tự nhiên", "ứng dụng thông minh"
+        "Khoa học Máy tính", "KHMT", "Trí tuệ nhân tạo", "AI", "máy học", 
+        "thị giác máy tính", "xử lý ngôn ngữ tự nhiên", "ứng dụng thông minh"
     ],
     "ktmt": [
-        "Kỹ thuật Máy tính", "KTMT", "thiết kế phần cứng", "hệ thống nhúng", "IoT", "robotics", "vi mạch"
+        "Kỹ thuật Máy tính", "KTMT", "thiết kế phần cứng", "hệ thống nhúng", 
+        "IoT", "robotics", "vi mạch"
     ],
     "ktpm": [
-        "Kỹ thuật phần mềm", "KTPM", "phát triển phần mềm", "lập trình", "kiểm thử phần mềm", "quản lý dự án phần mềm", "ứng dụng phần mềm"
+        "Kỹ thuật phần mềm", "KTPM", "phát triển phần mềm", "lập trình", 
+        "kiểm thử phần mềm", "quản lý dự án phần mềm"
     ],
     "mmtvttdl": [
-        "Mạng máy tính và truyền thông", "Internet of Things", "IoT", "mạng máy tính", "truyền thông dữ liệu", "kết nối mạng", "ứng dụng IoT", "bảo mật mạng"
+        "Mạng máy tính", "truyền thông dữ liệu", "Internet of Things", "IoT", 
+        "kết nối mạng", "ứng dụng IoT", "bảo mật mạng"
     ],
     "tkvm": [
-        "Thiết kế Vi Mạch", "vi mạch", "chip điện tử", "mạch tích hợp", "thiết kế IC", "linh kiện bán dẫn", "mô phỏng vi mạch"
+        "Thiết kế Vi Mạch", "vi mạch", "chip điện tử", "mạch tích hợp", "thiết kế IC", 
+        "linh kiện bán dẫn", "mô phỏng vi mạch"
     ],
     "tmdt": [
-        "Thương mại điện tử", "E-Commerce", "kinh doanh trực tuyến", "bán hàng trực tuyến", "tiếp thị trực tuyến", "quản trị TMĐT", "nền tảng TMĐT"
+        "Thương mại điện tử", "E-Commerce", "kinh doanh trực tuyến", "bán hàng trực tuyến", 
+        "tiếp thị trực tuyến", "quản trị TMĐT", "nền tảng TMĐT"
     ],
     "ttnt": [
-        "Trí tuệ nhân tạo", "AI", "máy học", "ứng dụng AI", "phân tích AI", "hệ thống thông minh", "học sâu"
+        "Trí tuệ nhân tạo", "AI", "máy học", "ứng dụng AI", "phân tích AI", 
+        "hệ thống thông minh", "học sâu"
     ],
     "ttdpt": [
-        "Truyền thông Đa phương tiện", "đa phương tiện", "sáng tạo nội dung", "tiếp thị số", "thiết kế đa phương tiện", "ứng dụng VR/AR", "quảng cáo trực tuyến"
+        "Truyền thông Đa phương tiện", "đa phương tiện", "sáng tạo nội dung", 
+        "tiếp thị số", "thiết kế đa phương tiện", "ứng dụng VR/AR", "quảng cáo trực tuyến"
     ],
+    "truong": [
+        "Trường Đại học Công nghệ Thông tin", "UIT", "ĐHQG-HCM", "khẩu hiệu", 
+        "tiền thân", "địa chỉ", "cơ sở vật chất", "tòa nhà", "Thư viện", "hiệu trưởng", 
+        "sinh viên", "ngành đào tạo", "khoa", "bộ môn", "Ban giám hiệu", "trình độ sau Đại học",
+        "thạc sĩ", "tiến sĩ", "giáo sư", "phó giáo sư", "lãnh đạo"
+    ],
+    "hoctap": [
+        "môn học", "tín chỉ", "tốt nghiệp", "khóa luận tốt nghiệp", "điều kiện tốt nghiệp",
+        "đầu ra", "chuẩn đầu ra", "giáo dục đại cương", "cơ sở ngành", "chuyên ngành",
+        "tự chọn", "tự chọn tự do", "kiến thức tốt nghiệp", "thực tập"
+    ],
+    "chungchi": [
+        "chứng chỉ ngoại ngữ", "chứng chỉ quốc tế", "SAT", "ACT", "IELTS",
+        "TOEFL", "JLPT"
+    ],
+    "giao_duc_pho_thong": [
+        "học sinh giỏi", "trường THPT", "trường chuyên", "trường năng khiếu", 
+        "hạnh kiểm", "điểm trung bình", "Olympic quốc tế", "Học sinh giỏi quốc gia",
+        "Đường lên đỉnh Olympia", "học sinh khá", "đội tuyển học sinh giỏi quốc gia"
+    ],
+    "giang_vien_va_can_bo": [
+        "giảng viên", "trợ giảng", "giáo sư", "phó giáo sư",
+        "tiến sĩ", "thạc sĩ", "Ths.", "Ths", "Ts.", "Ts", "TS.", 
+        "TS", "cán bộ", "cán bộ giảng dạy",
+        "trưởng khoa", "phó trưởng khoa", "bộ môn", "giảng dạy",
+        "ban giám hiệu", "trưởng bộ môn", "nhân sự học thuật",
+        "giảng viên cơ hữu", "giảng viên thỉnh giảng"
+    ],
+    "ngoai_le": [
+        "bootcamp", "sự kiện", "uni-tour", "tour", "hành trình",
+        "khám phá", "thcs", "thpt", "hoạt động ngoại khóa",
+        "trại hè", "định hướng nghề nghiệp", "talkshow", "workshop",
+        "chuỗi sự kiện", "giao lưu", "tư vấn tuyển sinh", "triển lãm giáo dục"
+    ]
 }
